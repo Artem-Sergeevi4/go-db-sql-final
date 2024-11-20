fd5b7a264f43321c44a9b22a304e9c7012d056de
Artem-Sergeevi4 <nokia990@bk.ru>
2024-11-20 16:23:27 +0300

    <first commit>

--- parcel.go
+++ parcel.go
@@ -4,57 +4,86 @@ import (
 	"database/sql"
 )
 
-type ParcelStore struct {
+type ParcelStore interface {
+	Add(parcel Parcel) (int, error)
+	GetByClient(client int) ([]Parcel, error)
+	Get(number int) (Parcel, error)
+	SetStatus(number int, status string) error
+	SetAddress(number int, address string) error
+	Delete(number int) error
+}
+
+type SQLiteParcelStore struct {
 	db *sql.DB
 }
 
-func NewParcelStore(db *sql.DB) ParcelStore {
-	return ParcelStore{db: db}
+func NewParcelStore(db *sql.DB) *SQLiteParcelStore {
+	return &SQLiteParcelStore{db: db}
 }
 
-func (s ParcelStore) Add(p Parcel) (int, error) {
-	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-
-	// верните идентификатор последней добавленной записи
-	return 0, nil
+func (s *SQLiteParcelStore) Add(parcel Parcel) (int, error) {
+	stmt, err := s.db.Prepare("INSERT INTO parcel (client, status, address, created_at) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	res, err := stmt.Exec(parcel.Client, parcel.Status, parcel.Address, parcel.CreatedAt)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	return int(id), err
 }
 
-func (s ParcelStore) Get(number int) (Parcel, error) {
-	// реализуйте чтение строки по заданному number
-	// здесь из таблицы должна вернуться только одна строка
+func (s *SQLiteParcelStore) GetByClient(client int) ([]Parcel, error) {
+	rows, err := s.db.Query("SELECT number, client, status, address, created_at FROM parcel WHERE client = ?", client)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-	// заполните объект Parcel данными из таблицы
-	p := Parcel{}
-
-	return p, nil
+	var parcels []Parcel
+	for rows.Next() {
+		var parcel Parcel
+		if err := rows.Scan(&parcel.Number, &parcel.Client, &parcel.Status, &parcel.Address, &parcel.CreatedAt); err != nil {
+			return nil, err
+		}
+		parcels = append(parcels, parcel)
+	}
+	return parcels, nil
 }
 
-func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
-	// реализуйте чтение строк из таблицы parcel по заданному client
-	// здесь из таблицы может вернуться несколько строк
-
-	// заполните срез Parcel данными из таблицы
-	var res []Parcel
-
-	return res, nil
+func (s *SQLiteParcelStore) Get(number int) (Parcel, error) {
+	row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = ?", number)
+	var parcel Parcel
+	if err := row.Scan(&parcel.Number, &parcel.Client, &parcel.Status, &parcel.Address, &parcel.CreatedAt); err != nil {
+		return Parcel{}, err
+	}
+	return parcel, nil
 }
 
-func (s ParcelStore) SetStatus(number int, status string) error {
-	// реализуйте обновление статуса в таблице parcel
-
-	return nil
+func (s *SQLiteParcelStore) SetStatus(number int, status string) error {
+	stmt, err := s.db.Prepare("UPDATE parcel SET status = ? WHERE number = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(status, number)
+	return err
 }
 
-func (s ParcelStore) SetAddress(number int, address string) error {
-	// реализуйте обновление адреса в таблице parcel
-	// менять адрес можно только если значение статуса registered
-
-	return nil
+func (s *SQLiteParcelStore) SetAddress(number int, address string) error {
+	stmt, err := s.db.Prepare("UPDATE parcel SET address = ? WHERE number = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(address, number)
+	return err
 }
 
-func (s ParcelStore) Delete(number int) error {
-	// реализуйте удаление строки из таблицы parcel
-	// удалять строку можно только если значение статуса registered
-
-	return nil
+func (s *SQLiteParcelStore) Delete(number int) error {
+	stmt, err := s.db.Prepare("DELETE FROM parcel WHERE number = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(number)
+	return err
 }
